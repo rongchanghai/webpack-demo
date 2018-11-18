b3a98a21b0f504db623a0b1d4a5442444e159116
changhai.rong <changhai.rong@wormpex.com>
2018-11-18 11:59:37 +0800

    使用dll-link 替换原始操作

--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,24 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const bundleConfig = require("./bundle-config.json");
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const DllLinkPlugin = require("dll-link-webpack-plugin");
+// const bundleConfig = require("./bundle-config.json");
 
 module.exports = {
   entry: {
-    app: path.resolve(__dirname, 'src', 'index.js'),
+    app: path.resolve(__dirname, "src", "index.js")
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name]_[contenthash:12].js',
-    chunkFilename: '[name]_[contenthash:12].js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name]_[contenthash:12].js",
+    chunkFilename: "[name]_[contenthash:12].js"
     // library: '[name]',
   },
   resolve: {
     extensions: [".js", ".jsx", ".css", ".less", ".json"],
     alias: {
-      $component: path.resolve('./', 'src/component'),
-    },
+      $component: path.resolve("./", "src/component")
+    }
   },
   // 基础目录，绝对路径，用于从配置中解析入口起点(entry point) 和 loader
   context: path.resolve(__dirname),
@@ -26,29 +27,26 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         // 可以是正则表达式，可以是绝对路径的字符串，还可以是个函数，数组
-        exclude: path.resolve(__dirname, 'node_modules'),  //  表示哪些目录中的文件不要进行 loader处理
-        include: path.resolve(__dirname, 'src'),
-        use: [
-          'babel-loader',
-          'lazyload-loader'
-        ]
+        exclude: path.resolve(__dirname, "node_modules"), //  表示哪些目录中的文件不要进行 loader处理
+        include: path.resolve(__dirname, "src"),
+        use: ["babel-loader", "lazyload-loader"]
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg|svgz)(\?.+)?$/,
-        include: path.join(__dirname, './src'),
+        include: path.join(__dirname, "./src"),
         exclude: /node_modules/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 10000
             }
-          },
+          }
         ]
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: 'file-loader'
+        use: "file-loader"
       }
     ]
   },
@@ -60,13 +58,13 @@ module.exports = {
         // 处理入口chunk
         commons: {
           test: /[\\/]src[\\/]component[\\/]/,
-          name: 'commons',
+          name: "commons",
           // minSize: 30000,
           // minChunks: 3,
-          chunks: 'all',
+          chunks: "all",
           priority: -1,
           reuseExistingChunk: true // 这个配置允许我们使用已经存在的代码块
-        },
+        }
         //  是否将common下的 组件的css单独打包出来，开启下面配置
         // styles: {
         //   name: 'styles',
@@ -75,7 +73,7 @@ module.exports = {
         //   enforce: true
         // }
       }
-    },
+    }
     // 为每个入口提取出webpack runtime模块
     // runtimeChunk: true,
     // 等价于
@@ -84,22 +82,24 @@ module.exports = {
     // }
   },
   plugins: [
-    //  使用dll来打包第三方代码
-    new webpack.DllReferencePlugin({
-      context: __dirname,
-      manifest: require('./dist/vendors-manifest.json')
+    //  使用dll来打包第三方代码  被dll-link  替换了
+    // new webpack.DllReferencePlugin({
+    //   context: __dirname,
+    //   manifest: require('./dist/vendors-manifest.json')
+    // }),
+    new DllLinkPlugin({
+      config: require("./webpack.dll.config.js"),
     }),
     new HtmlWebpackPlugin({
       // 如果配置此项 需要在HTML模板中的title标签中增加   <%= htmlWebpackPlugin.options.title %>
-      title: 'Webpack4',
+      title: "Webpack4",
       hash: true, //防止缓存
-      template: './src/www/index.html',
-      filename: 'index.html',
+      template: "./src/www/index.html",
+      filename: "index.html",
       // 使用 AssetsPlugin 打包生成的 json文件
-      bundleName: bundleConfig.vendors.js,
+      // bundleName: bundleConfig.vendors.js,
       // 需要将dll 文件链接到html 中
-      vendorsName: 'vendors.dll.js',
-    }),
-  ],
-
+      vendorsName: "vendors.dll.js"
+    })
+  ]
 };
--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack')
 const library = '[name]_lib'
 const path = require('path')
-const AssetsPlugin = require('assets-webpack-plugin');
+// const AssetsPlugin = require('assets-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
@@ -22,13 +22,13 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin('dist'),
     new webpack.DllPlugin({
-      path: path.join(__dirname, 'dist/vendors-manifest.json'),
+      path: path.join(__dirname, 'dist/[name]-manifest.json'),
       // This must match the output.library option above
       name: library
     }),
-    new AssetsPlugin({
-      filename: 'bundle-config.json',
-      // path: path.resolve(process.cwd(), 'dist'),
-    }),
+    // new AssetsPlugin({
+    //   filename: 'bundle-config.json',
+    //   // path: path.resolve(process.cwd(), 'dist'),
+    // }),
   ],
 }
